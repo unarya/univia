946114a86a547d7cf00422f02494d4a1d4d298bf
VuongMMO <tacongvuong1106@gmail.com>
2024-12-31 15:07:05 +0700

    update-get-user

--- modules/user/controllers/user.controller.go
+++ modules/user/controllers/user.controller.go
@@ -12,16 +12,29 @@ func GetUsers(c *gin.Context) {
 	// Chỉ gọi service để lấy dữ liệu
 	users, err := services.GetAllUsers()
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch users from the database"})
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status": gin.H{
+				"code":    http.StatusInternalServerError,
+				"message": "Failed to create user",
+			},
+			"error": err.Error(),
+		})
 		return
 	}
-
 	// Trả về danh sách người dùng
-	c.JSON(http.StatusOK, users)
+	c.JSON(http.StatusOK, gin.H{
+		"status": gin.H{
+			"code":    http.StatusOK,
+			"message": "Retrieved the list of users successfully",
+		},
+		"data": users,
+	})
+
 }
 
 func CreateUser(c *gin.Context) {
-	// Step 1: Parse JSON request body into `user_data`
+	// Step 1: Parse JSON request body into `
+
 	var userData model.User
 	if err := c.ShouldBindJSON(&userData); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
